de375999b23c09b417876d5e82d8f55f083123da
shehin18 <92203325+shehin18@users.noreply.github.com>
2024-12-01 23:18:19 +0530

    .

--- uart_rx_tb.v
+++ uart_rx_tb.v
@@ -1,7 +1,6 @@
 `timescale 1ns/10ps
 
-
-module uart_tb ();
+module uart_rx_tb ();
 
 //testbench uses a 10 MHz clock
 //want to interface to 19200 baud UART
@@ -11,16 +10,14 @@ parameter CLKS_PER_BIT = 521;
 parameter BIT_PERIOD = 10416; //time in ns that each bit should be present
 
 
-reg [7:0] tx_in = 8'h0;
 reg i_clk = 0;
 reg rx_in;
 reg rst_n;
 
-wire tx_out;
 wire [7:0] rx_out;
 
 task UART_READ_BYTE; //task to serialize input data
-    input [7:0] data;
+input [7:0] data
     integer i;
     begin
       rx_in <= 1'b0; //send start bit
@@ -38,27 +35,17 @@ task UART_READ_BYTE; //task to serialize input data
     end
 endtask //UART_READ_BYTE
 
-
-uart_tx #(.CLKS_PER_BIT(CLKS_PER_BIT)) UART_TX
-        (.rst_n(rst_n),
-        .tx_clk(i_clk),
-        .tx_in(tx_in),
-        .tx_out(tx_out));
-
 uart_rx #(.CLKS_PER_BIT(CLKS_PER_BIT)) UART_RX
         (.rst_n(rst_n),
         .rx_clk(i_clk),
         .rx_in(rx_in),
         .rx_out(rx_out));
 
-always //generating clock with 50% duty cycle
-#(CLK_PERIOD/2) i_clk <= ~i_clk;
+always #(CLK_PERIOD/2) i_clk <= ~i_clk; //generating clock with 50% duty cycle
 
 // Testbench sequence
 initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars;
-
+    
     rst_n = 0;
 
     #(CLK_PERIOD * 2);
@@ -68,13 +55,17 @@ initial begin
     UART_READ_BYTE(8'hE3); //send byte 0xE3
 
     // Wait and check if data received matches
-    #(BIT_PERIOD * 10);
+    @(posedge i_clk)
     if (rx_out == 8'hE3)
     $display ("Correct data received");
     else
     $display ("Incorrect data received");
-    $finish;
-
-end
+    $finish();
+    end
+    
+    initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    end
 
 endmodule
