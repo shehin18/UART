ed7d28a74c74808bf9c9a445528d578e28956e4e
shehin18 <92203325+shehin18@users.noreply.github.com>
2024-12-01 11:32:44 +0530

    .

--- uart_rx.v
+++ uart_rx.v
@@ -6,8 +6,9 @@ module uart_rx
 #(parameter CLKS_PER_BIT = 521)
   (rx_in, rst_n, rx_clk, rx_out);
 
-input wire rx_in, rst_n; //data input
-input wire rx_clk; //clock signal
+input rx_in; //data input
+input rst_n; //reset
+input rx_clk; //clock signal
 output reg [7:0] rx_out; //8 bit data output
 
 parameter IDLE = 2'b00;
@@ -16,31 +17,31 @@ parameter DATA_BURST = 2'b10;
 parameter STOP = 2'b11;
 
 reg data = 1'b0; //to store the incoming data
-reg [7:0] rx_count = 0;
+reg [7:0] rx_count = 1'b0;
 reg [3:0] bitpos = 4'h0;
 reg [1:0] rx_state = IDLE;
 
 //double register approach to avoid metastability
 reg R1;
-  always @(posedge rx_clk or negedge rst_n) begin
-    if(!rst_n)
-    rx_count <= 0;
-    bitpos <= 0;
+
+always @(posedge rx_clk or negedge rst_n) begin
+    if(!rst_n) begin
+    rx_count <= 1'b0;
+    bitpos <= 4'h0;
     rx_state <= IDLE;
-    rx_out <= 0;
-    data <= 1;
+    rx_out <= 8'h0;
+    data <= 1'b1;
     end
     else begin  
     R1 <= rx_in;
-    data <= R1;
-    end    
+    data <= R1;    
     
     case (rx_state)
     
     IDLE : begin
-        data <= 1;
-        bitpos <= 0;
-        rx_count <= 0;
+        data <= 1'b1;
+        bitpos <= 1'b0;
+        rx_count <= 1'b0;
 
         if(data == 1'b0) //start bit received
             rx_state <= START;
@@ -51,7 +52,7 @@ reg R1;
     START : begin
         if (rx_count == (CLKS_PER_BIT - 1)/2) begin
             if (data == 1'b0) begin //making sure that start bit 0 is received
-                rx_count <= 0;
+                rx_count <= 1'b0;
                 rx_state <= DATA_BURST;
             end
             else
@@ -69,14 +70,14 @@ reg R1;
             rx_state <= DATA_BURST;
         end
         else begin
-            rx_count <= 0;
+            rx_count <= 1'b0;
             rx_out[bitpos] <= data;
             if (bitpos < 3'h7) begin //check if all bits are received
                 bitpos <= bitpos + 1'b1;
                 rx_state <= DATA_BURST;
             end
             else begin
-                bitpos <= 0;
+                bitpos <= 1'b0;
                 rx_state <= STOP;
             end
         end
@@ -88,11 +89,12 @@ reg R1;
             rx_state <= STOP;
         end
         else begin
-            rx_count <= 0;
+            rx_count <= 1'b0;
             rx_state <= IDLE;
         end
     end //STOP
     endcase
+    end
 end
 
 endmodule
